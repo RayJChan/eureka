d610adccd9ee6dfd8ccc86186b2c6f663fafdf47
ChenWeiJie <weijie_chan1993@163.com>
2018-08-21 15:33:55 +0800

    1

--- eureka-config/config-client/src/main/java/com/eureka/configclient/ConfigClientApplication.java
+++ eureka-config/config-client/src/main/java/com/eureka/configclient/ConfigClientApplication.java
@@ -3,12 +3,21 @@ package com.eureka.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
+
+    /**
+     * http://localhost:8881/actuator/bus-refresh
+     */
 
     public static void main(String[] args) {
 
@@ -17,8 +26,9 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
+
     @RequestMapping(value = "/hi")
-    public String hi(){
-        return  foo;
+    public String hi() {
+        return foo;
     }
 }
